7a241a77ce5e997a12ef2dfd030304b2cd2bbd3a
Boye A. Høverstad <1488647+boyeah@users.noreply.github.com>
2024-12-29 09:24:41 +0100

    Modify threading class

--- src/pms7003/pms7003_threading.py
+++ src/pms7003/pms7003_threading.py
@@ -1,43 +1,33 @@
-import time
 from threading import Lock, Thread
 
-from .pms7003 import Measurement, Pms7003Sensor
+from .pms7003 import Pms7003Sensor
 
 
 class Pms7003Thread(Thread):
     def __init__(self, serial_device):
         super(Pms7003Thread, self).__init__()
-
+        self._lock = Lock()
         self._sensor = Pms7003Sensor(serial_device)
-        self._sensor_lock = Lock()
-
-        self._m: None | Measurement = None
-        self._m_lock = Lock()
-
+        self._measurements = []
         self._running = True
 
-    # Stuff for context manager
     def __enter__(self):
         self.start()
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
         self._running = False
-        with self._sensor_lock:
+        with self._lock:
             self._sensor.close()
 
     def run(self):
         while self._running:
-            with self._sensor_lock:
-                measurement = self._sensor.read_measurement()
-
-            with self._m_lock:
-                self._m = measurement
-
-            time.sleep(0.5)  # 2Hz (the sensor has 1Hz update)
+            with self._lock:
+                self._measurements.append(self._sensor.read_measurement())
 
     @property
     def measurements(self):
-        with self._m_lock:
-            val = self._m
+        with self._lock:
+            val = self._measurements
+            self._measurements = []
         return val
