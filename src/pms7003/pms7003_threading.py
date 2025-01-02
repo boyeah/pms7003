0c1d77759aad7dca7081fe681ad25f9192a4c079
Boye A. Høverstad <1488647+boyeah@users.noreply.github.com>
2025-01-02 09:00:17 +0100

    Accept read failures. Rewrite checks for running to avoid joining twice.

--- src/pms7003/pms7003_threading.py
+++ src/pms7003/pms7003_threading.py
@@ -1,16 +1,17 @@
 from threading import Lock, Thread
 
-from .pms7003 import Pms7003Sensor
+from .pms7003 import Pms7003Sensor, PmsSensorException
 from .utils import logger
 
 
 class Pms7003Thread(Thread):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, max_failures: int = -1, *args, **kwargs):
         super().__init__()
         self._lock = Lock()
         self._sensor = self._create_sensor(*args, **kwargs)
         self._measurements = []
-        self._running = True
+        self._running = False
+        self._max_failures = max_failures
 
     def _create_sensor(self, *args, **kwargs):
         return Pms7003Sensor(*args, **kwargs)
@@ -24,6 +25,8 @@ class Pms7003Thread(Thread):
 
     def stop(self):
         with self._lock:
+            if not self._running:
+                return
             self._running = False
         self.join()
         self._sensor.close()
@@ -32,11 +35,20 @@ class Pms7003Thread(Thread):
     def run(self):
         logger.info("PMS7003 sensor thread started")
         with self._lock:
-            running = self._running
+            running = self._running = True
+        _num_failures = 0
         while running:
             with self._lock:
-                measurement = self._sensor.read_measurement()
-                self._measurements.append(measurement)
+                try:
+                    measurement = self._sensor.read_measurement()
+                    self._measurements.append(measurement)
+                except PmsSensorException as e:
+                    _num_failures += 1
+                    if _num_failures == self._max_failures:
+                        raise PmsSensorException(
+                            f"Max failures reached ({self._max_failures})"
+                        ) from e
+                    logger.warning("Failed to read measurement: %s", e)
                 running = self._running
 
     def get_measurements(self):
